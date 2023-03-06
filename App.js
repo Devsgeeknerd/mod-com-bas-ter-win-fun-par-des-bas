3192454059cb0ab7ca93ab966f201a3860be7702
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-06 15:11:58 -0300

    add main stack

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 export default () => {
   return (
     <userContextProvider>
-      <NavigationContainer></NavigationContainer>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
     </userContextProvider>
   );
 };
