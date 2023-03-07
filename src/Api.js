aaa493f6e8588cf2eb3e39bd944b5275d4ada993
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-07 14:22:40 -0300

    add check token

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,16 @@
 const BASE_API = "https://api.b7web.com/devbarber/api";
 
-export default {};
+export default {
+  checkToken: async (token) => {
+    const req = await fetch(`${BASE_API}/auth/refresh`, {
+      method: "POST",
+      headers: {
+        Accept: "application.json",
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({ token }),
+    });
+    const json = await req.json();
+    return json;
+  },
+};
