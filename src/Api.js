5bd2db87765ef420be24d09ac9a4f98a4191160e
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-07 14:29:54 -0300

    add sign in

--- src/Api.js
+++ src/Api.js
@@ -13,4 +13,16 @@ export default {
     const json = await req.json();
     return json;
   },
+  signIn: async (email, password) => {
+    const req = await fetch(`${BASE_API}/auth/login`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await req.json();
+    return json;
+  },
 };
