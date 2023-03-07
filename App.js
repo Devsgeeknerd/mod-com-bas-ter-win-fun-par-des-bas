310a558997b530cd7ab0fa680a9c7ba94400f321
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-07 13:58:47 -0300

    atualizando informacoes

---
+++ App.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+import UserContextProvider from "./src/contexts/UserContext";
+import MainStack from "./src/stacks/MainStack";
+
+export default () => {
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </UserContextProvider>
+  );
+};
