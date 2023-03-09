495afef2ea2bc6fa606b954731306457dfa06356
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-09 13:58:47 -0300

    add update user

--- src/Api.js
+++ src/Api.js
@@ -158,5 +158,21 @@ export default {
 
     return json;
   },
-  updateUser: async () => {},
+  updateUser: async (body) => {
+    const token = await AsyncStorage.getItem("token");
+    body.token = token;
+
+    const req = await fetch(`${BASE_API}/user`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const json = await req.json();
+
+    return json;
+  },
 };
