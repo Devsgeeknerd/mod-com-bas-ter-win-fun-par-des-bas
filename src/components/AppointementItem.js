6f32035513ce3bd67188f2da84b6fc01dd3434e7
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-09 14:16:35 -0300

    add userArea

--- src/components/AppointementItem.js
+++ src/components/AppointementItem.js
@@ -1,6 +1,14 @@
+import styled from "styled-components/native";
+
 const Area = styled.View`
   background-color: #ffffff;
   margin-bottom: 20px;
   padding: 15px;
   border-radius: 20px;
+`;
+
+const UserArea = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
 `;
