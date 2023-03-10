9e77afa92c937b71224fa8eeecaa0461498b73af
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-10 15:30:34 -0300

    add area

---
+++ src/components/BarberItem.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+// My
+
+const Area = styled.TouchableOpacity`
+  background-color: #ffffff;
+  margin-bottom: 20px;
+  padding: 15px;
+  border-radius: 20px;
+  flex-direction: row;
+`;
