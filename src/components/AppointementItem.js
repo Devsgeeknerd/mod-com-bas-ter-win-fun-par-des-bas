2c74c9c55cb6d1db06e9ab79114b9d661da87514
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-10 15:02:18 -0300

    add export defaut and return

--- src/components/AppointementItem.js
+++ src/components/AppointementItem.js
@@ -46,3 +46,41 @@ const DateText = styled.Text`
   font-size: 16px;
   font-weight: bold;
 `;
+
+export default ({ data }) => {
+  let d = data.datetime.split(" ");
+
+  let time = d[1].substring(0.5);
+
+  let date = new Date(d[0]);
+
+  let year = date.getFullYear();
+
+  let month = date.getMonth();
+
+  let day = date.getDay();
+
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+
+  let dateString = `${day}/${month}/${year}`;
+
+  return (
+    <Area>
+      <UserArea>
+        <Avatar source={{ uri: data.barber.avatar }} />
+        <UserName>{data.barber.name}</UserName>
+      </UserArea>
+
+      <SplitArea>
+        <ServiceText>{data.barber.name}</ServiceText>
+        <ServiceText>R$ {data.service.price.toFixed(2)}</ServiceText>
+      </SplitArea>
+
+      <SplitArea>
+        <DateText>{dateString}</DateText>
+        <DateText>{time}</DateText>
+      </SplitArea>
+    </Area>
+  );
+};
