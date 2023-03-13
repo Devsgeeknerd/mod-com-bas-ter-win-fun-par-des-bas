e643640429e4d159a44c465632ee3a40f2947e29
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-13 14:23:53 -0300

    add export default and return

--- src/components/BarberItem.js
+++ src/components/BarberItem.js
@@ -1,6 +1,8 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
 import styled from "styled-components/native";
 
-// My
+import Stars from "../components/Stars";
 
 const Area = styled.TouchableOpacity`
   background-color: #ffffff;
@@ -29,13 +31,41 @@ const UserName = styled.Text`
 const SeeProfileButton = styled.View`
   width: 85px;
   height: 26px;
-  border: 1px solid #4ead4e;
+  border: 1px solid #4eadbe;
   border-radius: 10px;
   justify-content: center;
   align-items: center;
 `;
 
 const SeeProfileButtonText = styled.Text`
-  color: #262626;
+  color: #268596;
   font-size: 13px;
 `;
+
+export default ({ data }) => {
+  const navigation = useNavigation();
+
+  const handleClick = () => {
+    navigation.navigate("Barber", {
+      id: data.id,
+      avatar: data.avatar,
+      name: data.name,
+      stars: data.stars,
+    });
+  };
+
+  return (
+    <Area onPress={handleClick}>
+      <Avatar source={{ uri: data.avatar }} />
+      <InfoArea>
+        <UserName>{data.name}</UserName>
+
+        <Stars stars={data.stars} showNumber={true} />
+
+        <SeeProfileButton>
+          <SeeProfileButtonText>Ver Perfil</SeeProfileButtonText>
+        </SeeProfileButton>
+      </InfoArea>
+    </Area>
+  );
+};
