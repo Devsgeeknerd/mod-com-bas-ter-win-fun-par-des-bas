1dacff3572f4f3302558a4d29f50e59561da7ba0
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-14 14:58:56 -0300

    add ModalBody

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -7,3 +7,11 @@ const ModalArea = styled.View`
   flex: 1;
   justify-content: flex-end;
 `;
+
+const Modalbody = styled.View`
+  background-color: #83d6e3;
+  min-height: 300px;
+  padding: 10px 20px 40px 20px;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+`;
