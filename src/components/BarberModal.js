96581123260dd09c1e5bc17b0f3d74b271f81626
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-14 14:55:03 -0300

    add ModalArea

---
+++ src/components/BarberModal.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+const modal = styled.Modal``;
+
+const ModalArea = styled.View`
+  background-color: rgba(0, 0, 0, 0.5);
+  flex: 1;
+  justify-content: flex-end;
+`;
