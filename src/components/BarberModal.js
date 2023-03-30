a7677932a46c78a7831276d89c9890007157afce
Paulo Vanderley <vanderley.1109@gmail.com>
2023-03-30 13:18:34 -0300

    add setSelectedDay

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -161,5 +161,5 @@ export default () => {
 
   const [selectedYear, setSelectedYear] = useState(0);
   const [selectedMonth, setSelectedMonth] = useState(0);
-  const [selectedDay] = useState(0);
+  const [selectedDay, setSelectedDay] = useState(0);
 };
