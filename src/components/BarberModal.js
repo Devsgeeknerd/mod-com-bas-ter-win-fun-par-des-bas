efb048627ed8471fdf3f5707591ae6b12ec49a58
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-01 11:08:11 -0300

    add length

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -216,7 +216,7 @@ export default () => {
 
       let availability = user.available.filter((e) => e.date === selDate);
 
-      if (availability) {
+      if (availability.length > 0) {
       }
     }
   });
