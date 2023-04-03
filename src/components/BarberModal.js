5073322fa4b710c6bf3f8188362acbe494213c53
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-03 12:34:46 -0300

    add getMonth

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -236,6 +236,6 @@ export default () => {
     mountDate.setMonth(mountDate.getMonth() - 1);
 
     setSelectedYear(mountDate.getFullYear());
-    setSelectedMonth(mountDate);
+    setSelectedMonth(mountDate.getMonth());
   };
 };
