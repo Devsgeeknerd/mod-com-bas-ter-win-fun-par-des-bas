052d99c62d71aa3cc211baafde3d69896ce4d64f
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-03 11:39:42 -0300

    add getDate

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -228,6 +228,6 @@ export default () => {
 
     setSelectedYear(today.getFullYear());
     setSelectedMonth(today.getMonth());
-    setSelectedDay(today);
+    setSelectedDay(today.getDate());
   }, []);
 };
