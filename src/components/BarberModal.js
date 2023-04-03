ca389f74f05210267a8a072bfe978d920f71af62
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-03 11:55:11 -0300

    add selectedMonth

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -232,6 +232,6 @@ export default () => {
   }, []);
 
   const handleLeftDateClick = () => {
-    let mountDate = new Date(selectedYear);
+    let mountDate = new Date(selectedYear, selectedMonth, 1);
   };
 };
