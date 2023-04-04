4c7f97de065998f7b510eee923097eb9f9d6e285
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-04 11:34:49 -0300

    add selectedYear

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -241,7 +241,7 @@ export default () => {
   };
 
   const handleFinishClick = async () => {
-    if (user.if && service != null) {
+    if (user.if && service != null && selectedYear) {
     }
   };
 };
