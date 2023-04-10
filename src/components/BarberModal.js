57ffac7d773835288ed0d1c8f4bd58f571eadc35
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-10 11:45:59 -0300

    adicionando item

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -318,7 +318,7 @@ export default () => {
               </DateNextArea>
             </DateInfo>
             <DateList horizontal={true} showsHorizontalScrollIndicator={false}>
-              {listDays.map(() =>())}
+              {listDays.map((item) =>())}
             </DateList>
           </ModalItem>
         </Modalbody>
