2f24100289dd070c53b0837794fcb7d6ca9a3918
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-11 13:12:39 -0300

    adicionando horizontal

--- src/components/BarberModal.js
+++ src/components/BarberModal.js
@@ -352,7 +352,7 @@ export default () => {
           </ModalItem>
           {selectedDay > 0 && listHours.length > 0 && (
             <ModalItem>
-              <TimeList></TimeList>
+              <TimeList horizontal={}></TimeList>
             </ModalItem>
           )}
         </Modalbody>
