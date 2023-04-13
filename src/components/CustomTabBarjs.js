67411e94d959fa2a6059e4ca8b7d2e9b97946aa9
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-13 16:01:45 -0300

    adicionando fill

--- src/components/CustomTabBarjs.js
+++ src/components/CustomTabBarjs.js
@@ -58,7 +58,7 @@ export default ({ state, navigation }) => {
         <TodayIcon width="32" height="32" fill="#4eadbe" />
       </TabItemCenter>
       <TabItem onPress={() => goTo("Favorites")}>
-        <FavoriteIcon style={{}} width="24" height="24" />
+        <FavoriteIcon style={{}} width="24" height="24" fill="#ffffff" />
       </TabItem>
       <TabItem></TabItem>
     </TabArea>
