be11febd062b83f4ea603720cc93cfc0ea8de44e
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-14 10:42:17 -0300

    adicionando margin-bottom

--- src/components/SignInput.js
+++ src/components/SignInput.js
@@ -2,6 +2,7 @@ const InputArea = styled.View`
   background-color: #83d6e3;
   width: 100%;
   height: 60px;
+  margin-bottom: 15px;
   padding-left: 15px;
   border-radius: 30px;
   align-items: center;
