ec923a89a27e97c5913db8daadc66fc9969dae46
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-14 09:47:28 -0300

    adicionando AvatarIcon

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -65,7 +65,7 @@ export default ({ state, navigation }) => {
           fill="#ffffff"
         />
       </TabItem>
-      <TabItem onPress={() => goTo("Profile")}>{user.avatar}</TabItem>
+      <TabItem onPress={() => goTo("Profile")}>{user.avatar != '' ? <AvatarIcon/>}</TabItem>
     </TabArea>
   );
 };
