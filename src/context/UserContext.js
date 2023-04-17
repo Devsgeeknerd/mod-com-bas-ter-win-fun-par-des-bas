d24807ad2184fb9e0aac43519027dd99c3ed2c21
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-17 14:59:51 -0300

    adicionando children

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,6 +5,8 @@ export const UserContext = createContext();
 export default ({ children }) => {
   const [state, dispatch] = useReducer(UserReducer, initialState);
   return (
-    <UserContext.Provider value={{ state, dispatch }}></UserContext.Provider>
+    <UserContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserContext.Provider>
   );
 };
