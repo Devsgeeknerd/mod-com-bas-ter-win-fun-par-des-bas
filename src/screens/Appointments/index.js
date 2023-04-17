1ba4735c747c768c881cbf5b7792a9c0b6e48ddf
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-17 15:33:49 -0300

    adicionando false

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
 export default () => {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
   const [list, setList] = useState([]);
 };
