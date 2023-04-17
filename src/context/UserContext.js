ca9da3cb59da33bbe963d5ea0bc651dc8b4be1c0
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-17 15:03:25 -0300

    adicionando UserReducer

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import { initialState } from "../reducers/UserReducer";
+import { UserReducer, initialState } from "../reducers/UserReducer";
 
 export const UserContext = createContext();
 
