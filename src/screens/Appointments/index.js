fed8387fc8c68ca56979aa15cfe936d9bbec8592
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-17 15:33:15 -0300

    adicionando useState

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
 export default () => {
-  const[loading, setLoading]=useState();
-  const[list, setList];
+  const [loading, setLoading] = useState();
+  const [list, setList] = useState([]);
 };
