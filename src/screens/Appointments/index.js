6eb2db92201560b1cb5a4128e0499eb1dcdd52e6
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-18 10:39:41 -0300

    adicionando Scroller

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { RefreshControl } from "react-native";
-import { Container } from "./styles";
+import { Container, Scroller } from "./styles";
 
 export default () => {
   const [loading, setLoading] = useState(false);
