5cf2b0f05be81b1cfa9daf3a2afc6076c04ae1e0
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-18 10:50:22 -0300

    import api

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { RefreshControl } from "react-native";
+
 import { Container, Scroller, ListArea, EmptyWarning } from "./styles";
+import AppointmentItem from "../../components/AppointmentItem";
+import Api from "../../Api";
 
 export default () => {
   const [loading, setLoading] = useState(false);
