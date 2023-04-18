e40e1d3cff286c41edde8092f8d8abe4a462d120
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-18 10:38:14 -0300

    adicionando Conatiner

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { RefreshControl } from "react-native";
-import {} from "./styles";
+import { Container } from "./styles";
 
 export default () => {
   const [loading, setLoading] = useState(false);
