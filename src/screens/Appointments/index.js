0ac4f84cf762886f890499a2e8141cf7149f969f
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-18 10:36:18 -0300

    adicionando RefreshControl

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {} from "react-native";
+import { RefreshControl } from "react-native";
 
 export default () => {
   const [loading, setLoading] = useState(false);
