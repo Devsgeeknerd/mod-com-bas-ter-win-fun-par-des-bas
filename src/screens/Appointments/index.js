e96d63ab0d668042d7becf89540c9a48577d40bd
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-18 10:37:21 -0300

    importando styles

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { RefreshControl } from "react-native";
+import {} from "./styles";
 
 export default () => {
   const [loading, setLoading] = useState(false);
