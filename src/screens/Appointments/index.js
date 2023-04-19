231705381f5f198451b7c1ef122ed80f0f85e719
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-19 14:23:21 -0300

    adicionando data

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -39,7 +39,7 @@ export default () => {
         )}
         <ListArea>
           {list.map((item, k) => (
-            <AppointmentItem key={k} />
+            <AppointmentItem key={k} data={}/>
           ))}
         </ListArea>
       </Scroller>
