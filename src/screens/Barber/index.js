f9e5bf3ec03874bea1213f0040b74836b2a41f59
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-21 14:42:53 -0300

    adicionando setShowModal

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -13,5 +13,5 @@ export default () => {
   const [loading, setLoading] = useState(false);
   const [favorited, setFavorited] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
-  const [showModal] = useState(false);
+  const [showModal, serShowModal] = useState(false);
 };
