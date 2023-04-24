4aa9f0d7ea6f3b6a8c171fce3e4c10e996a641a2
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-24 11:39:48 -0300

    adicionando stars

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -117,7 +117,7 @@ export default () => {
             <UserAvatar source={{ uri: userInfo.avatar }} />
             <UserInfo>
               <UserInfoName>{userInfo.name}</UserInfoName>
-              <Stars stars={userInfo} />
+              <Stars stars={userInfo.stars} />
             </UserInfo>
           </UserInfoArea>
         </PageBody>
