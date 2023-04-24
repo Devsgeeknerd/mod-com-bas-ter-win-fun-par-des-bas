33d9e6a05ecac582d16c3ee07437fd35a367b504
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-24 11:42:20 -0300

    adicionando UserFavButton

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -119,6 +119,7 @@ export default () => {
               <UserInfoName>{userInfo.name}</UserInfoName>
               <Stars stars={userInfo.stars} showNumber={true} />
             </UserInfo>
+            <UserFavButton></UserFavButton>
           </UserInfoArea>
         </PageBody>
       </Scroller>
