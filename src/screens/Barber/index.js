23d2470909799d7dc85bbd1a5b62137f0bdcf0ee
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-25 10:05:15 -0300

    adicionando width

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -150,7 +150,7 @@ export default () => {
                 style={{ height: 110 }}
                 showsPagination={false}
                 showsButtons={true}
-                prevButton={<NavPrevIcon />}
+                prevButton={<NavPrevIcon width="35" />}
               ></Swiper>
             </TestimonialArea>
           )}
