5bc03a86b184f3004b64259ee9ef722e84f93ae8
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 11:21:01 -0300

    adiconando item

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -163,7 +163,7 @@ export default () => {
                       <TestimonialName>{item.name}</TestimonialName>
                       <Stars stars={item.rate} showNumber={false} />
                     </TestimonialInfo>
-                    <TestimonialBody></TestimonialBody>
+                    <TestimonialBody>{item}</TestimonialBody>
                   </TestimonialItem>
                 ))}
               </Swiper>
