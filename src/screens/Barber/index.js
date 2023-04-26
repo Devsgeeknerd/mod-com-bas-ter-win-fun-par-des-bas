7c94d1a9adcd35cc9195322c4d4b61a81abb13fd
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 10:52:55 -0300

    adicionando name

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -160,7 +160,7 @@ export default () => {
                 {userInfo.testimonials.map((item, key) => (
                   <TestimonialItem key={key}>
                     <TestimonialInfo>
-                      <TestimonialName>{item}</TestimonialName>
+                      <TestimonialName>{item.name}</TestimonialName>
                     </TestimonialInfo>
                   </TestimonialItem>
                 ))}
