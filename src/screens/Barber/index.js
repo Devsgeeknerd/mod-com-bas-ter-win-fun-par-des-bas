6b6c49befaafa35eccedb61a41473efc55a25eba
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 11:06:28 -0300

    adicionando onPress

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -170,7 +170,7 @@ export default () => {
           )}
         </PageBody>
       </Scroller>
-      <BackButton></BackButton>
+      <BackButton onPress={}></BackButton>
     </Container>
   );
 };
