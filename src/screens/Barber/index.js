1d736c5162ef81afc5dbd46f73e351d051e9288a
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 11:17:32 -0300

    adicionando selectedService

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -173,7 +173,12 @@ export default () => {
       <BackButton onPress={handleBackButton}>
         <BackIcon width="44" height="44" fill="#ffffff" />
       </BackButton>
-      <BarberModal show={showModal} setShow={setShowModal} user={userInfo}service={} />
+      <BarberModal
+        show={showModal}
+        setShow={setShowModal}
+        user={userInfo}
+        service={selectedService}
+      />
     </Container>
   );
 };
