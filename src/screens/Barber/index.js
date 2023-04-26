07502918c4dd55ced2f0b2e64cf72a38061ec623
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 11:01:09 -0300

    adicionando false

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -161,7 +161,7 @@ export default () => {
                   <TestimonialItem key={key}>
                     <TestimonialInfo>
                       <TestimonialName>{item.name}</TestimonialName>
-                      <Stars stars={item.rate} showNumber={}/>
+                      <Stars stars={item.rate} showNumber={false} />
                     </TestimonialInfo>
                   </TestimonialItem>
                 ))}
