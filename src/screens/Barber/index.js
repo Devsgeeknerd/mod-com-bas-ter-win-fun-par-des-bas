64db196dc80c35f6ab8efe4969253c07bdaa70e2
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 11:12:32 -0300

    adicionando barberModal

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -173,6 +173,7 @@ export default () => {
       <BackButton onPress={handleBackButton}>
         <BackIcon width="44" height="44" fill="#ffffff" />
       </BackButton>
+      <BarberModal />
     </Container>
   );
 };
