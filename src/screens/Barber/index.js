fb23e9002848a8c1ef7dcc560fed8ebe84d7bcab
Paulo Vanderley <vanderley.1109@gmail.com>
2023-04-26 10:54:20 -0300

    adicionando stars

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -161,7 +161,7 @@ export default () => {
                   <TestimonialItem key={key}>
                     <TestimonialInfo>
                       <TestimonialName>{item.name}</TestimonialName>
-                      <Stars />
+                      <Stars stars={}/>
                     </TestimonialInfo>
                   </TestimonialItem>
                 ))}
