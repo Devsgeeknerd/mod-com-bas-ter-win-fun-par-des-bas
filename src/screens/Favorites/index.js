b6e4921cd73a9899a01b5ead2feb7cb72ec7f3ad
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-02 10:57:38 -0300

    adicionando false

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -15,5 +15,5 @@ import BarberItem from "../../components/BarberItem";
 import api from "../../Api";
 
 export default () => {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
 };
