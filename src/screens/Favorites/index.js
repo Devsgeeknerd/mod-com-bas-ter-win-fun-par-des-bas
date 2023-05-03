a09ad5ce62d57fecfcb7fc488e02fffb7af19753
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-03 11:02:45 -0300

    adicionando map

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -49,7 +49,7 @@ export default () => {
         {!loading && list.length === 0 && (
           <EmptyWarning>Não ha favoritos.</EmptyWarning>
         )}
-        <ListArea>{list}</ListArea>
+        <ListArea>{list.map}</ListArea>
       </Scroller>
     </Container>
   );
