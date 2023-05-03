4bee0183fc265d0330fc56719a625dba1f7997a0
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-03 10:43:07 -0300

    adicionando refreshControl

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -41,7 +41,7 @@ export default () => {
       <HeaderArea>
         <HeaderTitle>Favoritos</HeaderTitle>
       </HeaderArea>
-      <Scroller></Scroller>
+      <Scroller refreshControl={}></Scroller>
     </Container>
   );
 };
