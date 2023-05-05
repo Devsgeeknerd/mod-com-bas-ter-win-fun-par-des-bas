c1474641379c279a774d62193c941f06aeb99328
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-05 10:17:04 -0300

    adicionando react-native-community

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,4 +2,4 @@ import React, { useState, useEffect } from "react";
 import { Platform, RefreshControl } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { request, PERMISSIONS } from "react-native-permissions";
-import Geolocation from "";
+import Geolocation from "@react-native-community/geolocation";
