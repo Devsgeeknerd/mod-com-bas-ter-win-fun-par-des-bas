55cb9bd663c9d462686bd8bea4f478c2b15c08fb
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-05 10:10:41 -0300

    adicionando resquest

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,4 +1,4 @@
 import React, { useState, useEffect } from "react";
 import { Platform, RefreshControl } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import
+import {request}
