073d0ea50a6ae3484e1876a9913f0a4aca060162
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-06 15:36:03 -0300

    adicionando info

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -46,7 +46,7 @@ export default () => {
       setLocationText("");
       setList([]);
 
-      Geolocation.getCurrentPosition(() => {});
+      Geolocation.getCurrentPosition((info) => {});
     }
   };
 };
