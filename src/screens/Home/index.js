33dea1ef7680a21a1c5be7242ec25163fc2c4fbf
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-06 15:28:17 -0300

    adicionando if

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -38,7 +38,9 @@ export default () => {
     let result = await request(
       Platform.OS === "ios"
         ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
-        : PERMISSIONS.ANDROID.ACCESS
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
     );
+
+    if(){}
   };
 };
