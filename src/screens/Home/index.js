47918b154b232504db3bb235d3bd99b5cbaa8e43
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-06 15:04:44 -0300

    adicionando const

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -30,4 +30,5 @@ export default () => {
   const [locationText, setLocationText] = useState("");
   const [coords, setCoords] = useState(null);
   const [Loading, setLoading] = useState(false);
+  const
 };
