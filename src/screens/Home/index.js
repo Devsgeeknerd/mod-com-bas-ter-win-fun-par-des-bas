da0e135ae8e4aad6c3d9bce61801566814cc21ee
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-06 15:22:58 -0300

    adicionando ios

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -35,6 +35,6 @@ export default () => {
 
   const handleLocationFinder = async () => {
     setCoords(null);
-    let result = await request(Platform.OS);
+    let result = await request(Platform.OS === "ios");
   };
 };
