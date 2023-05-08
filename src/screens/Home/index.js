280f163de67f9924f224c8c5106cef741169342c
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-08 14:16:01 -0300

    adicionando RefreshControl

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -93,7 +93,7 @@ export default () => {
 
   return (
     <Container>
-      <Scroller refreshControl></Scroller>
+      <Scroller refreshControl={<RefreshControl />}></Scroller>
     </Container>
   );
 };
