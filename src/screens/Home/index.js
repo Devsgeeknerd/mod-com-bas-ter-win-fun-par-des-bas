7388bdcd8a18ea178b2c367d5398a2166d582b1a
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-09 16:13:15 -0300

    adicionando map

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -119,7 +119,7 @@ export default () => {
           </LocationFinder>
         </LocationArea>
         {loading && <LoadingIcon size="large" color="#ffffff" />}
-        <ListArea>{list}</ListArea>
+        <ListArea>{list.map(()=>())}</ListArea>
       </Scroller>
     </Container>
   );
