f3b5e0cd42afc4401640a5861880992792cff399
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-10 10:56:04 -0300

    adicionando from

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
 import { Container, LoadingIcon } from "./styles";
-import AsyncStorage
+import AsyncStorage from "";
 
 export default () => {};
