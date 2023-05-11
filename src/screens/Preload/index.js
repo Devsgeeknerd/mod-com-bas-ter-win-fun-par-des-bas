94007be4d34907282cad3b96f853f8afed860407
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-11 10:59:23 -0300

    adicionando userContext

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -3,11 +3,11 @@ import { Container, LoadingIcon } from "./styles";
 import AsyncStorage from "@react-native-community/async-storage";
 import { useNavigation } from "@react-navigation/native";
 
-import { userContext } from "../../contexts/UserContext";
+import UserContext, { userContext } from "../../contexts/UserContext";
 import Api from "../../Api";
 
 import BarberLogo from "../../assets/barber.svg";
 
 export default () => {
-  const {} = useContext();
+  const {} = useContext(UserContext);
 };
