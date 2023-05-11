45fc51f1ac3b305f8c6b4bf9829952aa3c970db6
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-11 10:46:21 -0300

    adicionando react-navigation

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import { Container, LoadingIcon } from "./styles";
 import AsyncStorage from "@react-native-community/async-storage";
-import { useNavigation } from "";
+import { useNavigation } from "@react-navigation/native";
 
 export default () => {};
