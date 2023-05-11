3847b181ca0c2ff9847633225cab03ebe63f16fc
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-11 10:51:03 -0300

    adicionando import

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -4,5 +4,6 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { useNavigation } from "@react-navigation/native";
 
 import { userContext } from "../../contexts/UserContext";
+import
 
 export default () => {};
