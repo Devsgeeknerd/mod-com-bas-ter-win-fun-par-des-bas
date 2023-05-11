7f0e85b67c88c858fd64754018488afdecaf53bf
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-11 10:47:40 -0300

    adicionando from

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -3,6 +3,6 @@ import { Container, LoadingIcon } from "./styles";
 import AsyncStorage from "@react-native-community/async-storage";
 import { useNavigation } from "@react-navigation/native";
 
-import
+import {} from "";
 
 export default () => {};
