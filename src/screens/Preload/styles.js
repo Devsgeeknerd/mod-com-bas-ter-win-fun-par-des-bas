960a1930f83bf0b10cc984b4b85a67c81d98e761
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-12 10:19:40 -0300

    adicionando align-items

--- src/screens/Preload/styles.js
+++ src/screens/Preload/styles.js
@@ -4,5 +4,6 @@ import styled from "styled-components/native";
 export const Container = styled.SafeAreaView`
   background-color: #63c2d1;
   justify-content: center;
+  align-items: center;
   flex: 1;
 `;
