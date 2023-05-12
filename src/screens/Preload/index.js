9a36ff4b57bdae635ce88187ac097d028da3b019
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-12 10:09:14 -0300

    adicionando height

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -36,7 +36,7 @@ export default () => {
 
   return (
     <Container>
-      <BarberLogo width="100%" />
+      <BarberLogo width="100%" height="160" />
       <LoadingIcon />
     </Container>
   );
