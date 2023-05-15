57e3e69572e876ac98062f95de87fbfff23f6623
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-15 14:17:25 -0300

    adicionando value

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -45,6 +45,7 @@ export default () => {
         <SearchInput
           placeholder="Digite o nome do barbeiro"
           placeholderTextColor="#ffffff"
+          value={}
         />
       </SearchArea>
     </Container>
