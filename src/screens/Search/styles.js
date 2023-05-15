9729cd6b96a516bac8464eeaef486f04013d91c4
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-15 14:47:05 -0300

    adicionando background-color

--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,3 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  background-color: #63c2d1;
+  flex: 1;
+`;
