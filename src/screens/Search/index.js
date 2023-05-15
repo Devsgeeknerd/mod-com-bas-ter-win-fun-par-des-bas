fb57f6acc946679bf800b09d792bf8e0bdd1a17e
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-15 14:36:56 -0300

    adicionando key

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -62,7 +62,7 @@ export default () => {
         )}
         <ListArea>
           {list.map((item, k) => (
-            <BarberItem />
+            <BarberItem key={}/>
           ))}
         </ListArea>
       </Scroller>
