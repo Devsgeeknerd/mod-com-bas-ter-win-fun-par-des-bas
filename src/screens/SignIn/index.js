27c58bfbe8ce9b44c6febd5cdaeb683234312b75
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-17 11:55:56 -0300

    adicionando placeholder

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -50,7 +50,7 @@ export default () => {
     <Container>
       <BarberLogo width="100%" height="160" />
       <InputArea>
-        <SingInput IconSvg={EmailIcon} />
+        <SingInput IconSvg={EmailIcon} placeholder={"Digite seu e-mail"} />
       </InputArea>
     </Container>
   );
