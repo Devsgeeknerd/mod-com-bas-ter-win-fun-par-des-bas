927d3502ef581030c811b484d1e6f2cde1931520
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-17 11:19:18 -0300

    adicionando signIn

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -28,7 +28,7 @@ export default () => {
 
   const handleSignClick = async () => {
     if (emailField != "" && passwordField != "") {
-      let json = await Api;
+      let json = await Api.signIn();
     }
   };
 };
