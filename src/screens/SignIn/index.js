e995938ba821fb73f6d1eb9411b9439a418aae84
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-18 11:26:40 -0300

    adicionando onChangeText

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -60,6 +60,7 @@ export default () => {
           IconSvg={LockIcon}
           placeholder="Digite sua senha"
           value={passwordField}
+          onChangeText={}
         />
       </InputArea>
     </Container>
