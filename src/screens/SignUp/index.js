eb537c123bdd9971ed1006e88061a245209be78f
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-20 14:16:10 -0300

    adicionando styles

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -3,4 +3,4 @@ import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import { UserContext } from "../../contexts/UserContext";
-import {} from "";
+import {} from "./styles";
