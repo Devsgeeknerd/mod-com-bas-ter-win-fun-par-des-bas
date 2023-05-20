9134f8d436c7ef78590feaca691e6b1741b1fa69
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-20 13:57:03 -0300

    adiciionando Text

--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -38,4 +38,4 @@ export const SignMessageButtonText = styled.Text`
   color: #268596;
 `;
 
-export const SignMessageButtonTextBold = styled;
+export const SignMessageButtonTextBold = styled.Text``;
