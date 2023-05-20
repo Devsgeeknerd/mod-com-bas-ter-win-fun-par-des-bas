cf102122f68133d0b02c7507924c28c27b94b49a
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-20 14:08:51 -0300

    adicionando async-storage

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,3 +1,3 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import AsyncStorage from "";
+import AsyncStorage from "@react-native-community/async-storage";
