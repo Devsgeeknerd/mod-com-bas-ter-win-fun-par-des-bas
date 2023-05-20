f63f01a6dd6aa6e8d3674fde85c3dfa9f1e25198
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-20 14:17:32 -0300

    adicionando inputArea

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -3,4 +3,4 @@ import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import { UserContext } from "../../contexts/UserContext";
-import { Container } from "./styles";
+import { Container, InputArea } from "./styles";
