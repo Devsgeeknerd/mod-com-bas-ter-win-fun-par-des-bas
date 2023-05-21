bb20c97c6839530b72967c9bd7ec463db4edf089
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-21 13:40:27 -0300

    adicionando if

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -32,6 +32,7 @@ export default () => {
   const handleSignClick = async () => {
     if (nameField != "" && emailField != "" && passwordField != "") {
       let res = await Api.signUp(nameField, emailField, passwordField);
+      if(){}
     }
   };
 };
