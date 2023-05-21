e4163e0c6a3e2419e2bee6f7acd5447c12682c10
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-21 13:54:03 -0300

    adicionando data

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -34,7 +34,7 @@ export default () => {
       let res = await Api.signUp(nameField, emailField, passwordField);
       if (res.token) {
         await AsyncStorage.setItem("token", res.token);
-        userDispatch({ type: "setAvatar", payload: { avatar: res } });
+        userDispatch({ type: "setAvatar", payload: { avatar: res.data } });
       }
     }
   };
