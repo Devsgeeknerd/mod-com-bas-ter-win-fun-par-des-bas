2b01192cffef2bc66973c50ca8a7bdd59f7b9b27
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-21 13:34:03 -0300

    adicionando emailField

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -30,7 +30,7 @@ export default () => {
   const [passwordField, setPasswordField] = useState("");
 
   const handleSignClick = async () => {
-    if (nameField) {
+    if (nameField != "" && emailField) {
     }
   };
 };
