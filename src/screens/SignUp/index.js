798c588da6fa4ea220dbbd8758df1eada6ed6482
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-22 11:22:53 -0300

    adicionando height

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -53,7 +53,7 @@ export default () => {
 
   return (
     <Container>
-      <BarberLogo width="100%" />
+      <BarberLogo width="100%" height="160" />
       <SignInput IconSvg={PersonIcon} />
     </Container>
   );
