e11aeeb0a3cc160a8d306fc737839b13a78f7dd9
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-22 11:38:43 -0300

    adicionando placeholder

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -55,7 +55,7 @@ export default () => {
     <Container>
       <BarberLogo width="100%" height="160" />
       <InputArea>
-        <SignInput IconSvg={PersonIcon} />
+        <SignInput IconSvg={PersonIcon} placeholder="Digite seu nome" />
       </InputArea>
     </Container>
   );
