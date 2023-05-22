ee8d71009a18090ae1253a793f4b6da3d608fcb3
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-22 11:51:16 -0300

    adicionando placeholder

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -67,7 +67,7 @@ export default () => {
           value={emailField}
           onChangeText={(t) => setEmailField(t)}
         />
-        <SignInput IconSvg={LockIcon} />
+        <SignInput IconSvg={LockIcon} placeholder="Digite sua senha" />
       </InputArea>
     </Container>
   );
