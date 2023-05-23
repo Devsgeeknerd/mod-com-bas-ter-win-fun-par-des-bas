ec1fdf4a554ebaab2a6aba246b7a6c9b72d5e29c
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-23 13:03:46 -0300

    adicionando customButtonText

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -74,7 +74,9 @@ export default () => {
           onChangeText={(t) => setPasswordField(t)}
           password={true}
         />
-        <CustomButton onPress={handleSignClick}></CustomButton>
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtonText></CustomButtonText>
+        </CustomButton>
       </InputArea>
     </Container>
   );
