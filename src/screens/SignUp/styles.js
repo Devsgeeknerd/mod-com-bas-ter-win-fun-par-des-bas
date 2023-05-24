5ed697b648e8266896f19ab70d5ddcfc7ecc65c4
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-24 11:57:09 -0300

    adicionando text

--- src/screens/SignUp/styles.js
+++ src/screens/SignUp/styles.js
@@ -33,4 +33,4 @@ export const SignMessageButton = styled.TouchableOpacity`
   flex-direction: row;
 `;
 
-export const SignMessageButtonText = styled;
+export const SignMessageButtonText = styled.Text``;
