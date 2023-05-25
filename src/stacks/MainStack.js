4b42ce60cc6e9f15d8e34749ab0456b3c506908e
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:09:04 -0300

    adicionando signUp

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -3,4 +3,4 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Preload from "../screens/Preload";
 import SignIn from "../screens/SignIn";
-import SignUp from "";
+import SignUp from "../screens/SignUp";
