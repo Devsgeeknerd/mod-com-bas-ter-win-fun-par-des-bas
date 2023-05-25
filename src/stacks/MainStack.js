d321f11b6a54da16b00aec1e4f67249fea2d1ea3
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:32:19 -0300

    adicionando component

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -14,6 +14,6 @@ export default () => (
     initialRouteName="Preload"
     screenOptions={{ headerShow: false }}
   >
-    <Stack.Screen name="Preload" />
+    <Stack.Screen name="Preload" component={}/>
   </Stack.Navigator>
 );
