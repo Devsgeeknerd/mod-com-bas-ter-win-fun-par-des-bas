959202ad5446d894d1a3870a29d789d56ea54b2b
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:45:53 -0300

    adicionando screen

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -18,6 +18,6 @@ export default () => (
     <Stack.Screen name="SignIn" component={SignIn} />
     <Stack.Screen name="SignUp" component={SignUp} />
     <Stack.Screen name="MainTab" component={MainTab} />
-    <Stack />
+    <Stack.Screen />
   </Stack.Navigator>
 );
