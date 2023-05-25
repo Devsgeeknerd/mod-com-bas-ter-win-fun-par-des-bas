1530eaec61b18567fb8cc9c4be1a917589490b68
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:41:34 -0300

    adicionando component

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -16,6 +16,6 @@ export default () => (
   >
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SignIn" component={SignIn} />
-    <Stack.Screen name="SignUp" />
+    <Stack.Screen name="SignUp" component={}/>
   </Stack.Navigator>
 );
