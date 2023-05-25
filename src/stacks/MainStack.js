0c43ea3cfa330eaa532606165d997ce0919d03fa
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:10:16 -0300

    adicionando from

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -4,4 +4,4 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Preload from "../screens/Preload";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
-import MainTab
+import MainTab from "";
