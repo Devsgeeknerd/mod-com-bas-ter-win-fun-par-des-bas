127de777b7e84be83b7a4afbce2e29170757bceb
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:44:03 -0300

    adicionando mainTab

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -17,6 +17,6 @@ export default () => (
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SignIn" component={SignIn} />
     <Stack.Screen name="SignUp" component={SignUp} />
-    <Stack.Screen name="" />
+    <Stack.Screen name="MainTab" />
   </Stack.Navigator>
 );
