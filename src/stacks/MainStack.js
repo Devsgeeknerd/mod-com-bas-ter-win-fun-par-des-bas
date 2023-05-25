bde34fb45fce2949863b0e654d145659bb360ac0
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-25 11:39:48 -0300

    adicionando signUp

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -16,6 +16,6 @@ export default () => (
   >
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SignIn" />
-    <Stack.Screen name="" />
+    <Stack.Screen name="SignUp" />
   </Stack.Navigator>
 );
