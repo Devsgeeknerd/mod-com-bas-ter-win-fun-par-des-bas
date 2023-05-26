01cab8260a63eeadbccaf69428a61b5ad7126d33
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-26 11:23:15 -0300

    adicionando props

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -11,4 +11,4 @@ import Profile from "../screens/Profile";
 
 const Tab = createBottomNavigator();
 
-export default () => <Tab.Navigator tabBar={}></Tab.Navigator>;
+export default () => <Tab.Navigator tabBar={props}></Tab.Navigator>;
