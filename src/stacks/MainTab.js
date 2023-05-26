cef9bb1728ed5ebb0a5f1218a8f7f5e93cc47d26
Paulo Vanderley <vanderley.1109@gmail.com>
2023-05-26 11:04:55 -0300

    adicionando customTabBar

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -1,4 +1,4 @@
 import React from "react";
 import { createBottomNavigator } from "@react-navigation/bottom-tabs";
 
-import CustomTabBar from "";
+import CustomTabBar from "../components/CustomTabBar";
